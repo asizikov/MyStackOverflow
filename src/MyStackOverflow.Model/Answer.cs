df9f949c0078194b9feb80f87ab593a68f346cf1
Anton Sizikov <sizikov.anton@gmail.com>
2014-01-15 22:24:59 +0100

    answers responce model

---
+++ src/MyStackOverflow.Model/Answer.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+
+namespace MyStackOverflow.Model
+{
+    public class Answer
+    {
+        [JsonProperty("answer_id")]
+        public int AnswerID { get; set; }
+
+        [JsonProperty("accepted")]
+        public bool Accepted { get; set; }
+
+        [JsonProperty("answer_comments_url")]
+        public string AnswerCommentsUrl { get; set; }
+
+        [JsonProperty("question_id")]
+        public int QuestionID { get; set; }
+
+        [JsonProperty("creation_date")]
+        public int CreationDate { get; set; }
+
+        [JsonProperty("last_activity_date")]
+        public int LastActivityDate { get; set; }
+
+        [JsonProperty("up_vote_count")]
+        public int UpVoteCount { get; set; }
+
+        [JsonProperty("down_vote_count")]
+        public int DownVoteCount { get; set; }
+
+        [JsonProperty("view_count")]
+        public int ViewCount { get; set; }
+
+        [JsonProperty("score")]
+        public int Score { get; set; }
+
+        [JsonProperty("community_owned")]
+        public bool CommunityOwned { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("last_edit_date")]
+        public int? LastEditDate { get; set; }
+    }
+}
---
+++ src/MyStackOverflow.Model/AnswersResponce.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace MyStackOverflow.Model
+{
+    public class AnswersResponce
+    {
+        [JsonProperty("total")]
+        public int Total { get; set; }
+        [JsonProperty("page")]
+        public int Page { get; set; }
+        [JsonProperty("pagesize")]
+        public int Pagesize { get; set; }
+        [JsonProperty("answers")]
+        public List<Answer> Answers { get; set; }
+    }
+}
