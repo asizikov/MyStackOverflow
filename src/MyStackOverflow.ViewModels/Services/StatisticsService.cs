ea4f69f8f9423c9801f6efbe8882e8ada0c3995d
unknown <sizikov.anton@gmail.com>
2014-01-22 21:21:54 +0100

    statistics added

--- src/MyStackOverflow.ViewModels/Services/StatisticsService.cs
+++ src/MyStackOverflow.ViewModels/Services/StatisticsService.cs
@@ -3,6 +3,7 @@
     public abstract class StatisticsService
     {
         private const string  PROFILE_PAGE_LOADED = "Profile page loaded";
+        private const string  ACTIVITY_LOADED = "Activity page loaded:";
         private const string LOGIN_PAGE_LOADED = "Login page loaded";
 
         protected abstract void PublishEvent(string eventName);
@@ -15,6 +16,11 @@
         public void ReportProfilePageLoaded()
         {
             PublishEvent(PROFILE_PAGE_LOADED);
+        }
+
+        public void PublishActivityPageLoaded(bool isQuestions)
+        {
+            PublishEvent(ACTIVITY_LOADED + (isQuestions ? "Questions" : "Answers"));
         }
     }
 }
